729f0009d46bdfae94b3e3695e6c74fc2803fe00
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2022-12-06 16:35:31 +0300

    Admin Profile & Image Update Part 5

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -6,18 +6,18 @@
    <div class="container-fluid">
  
 
-<div class="row">
+                    <div class="row">
                             <div class="col-lg-4">
                                 <div class="card"><br><br>
                                     <center>
-                                    <img class="rounded-circle avatar-xl" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                                    <img class="rounded-circle avatar-xl" src="{{(!empty($adminData->profile_image)) ? url('upload/admin_images/'.$adminData->profile_image ): url('upload/no_image.jpg')  }}" alt="Card image cap"> 
                                     </center>
                                     <div class="card-body">
                                         <h4 class="card-title">Name : {{$adminData->name}}</h4>
                                         <hr>
-                                       <h4 class="card-title">Name : {{$adminData->email}}</h4>
+                                       <h4 class="card-title">Email : {{$adminData->email}}</h4>
                                        <hr>
-                                       <h4 class="card-title">Name : {{$adminData->username}}</h4>
+                                       <h4 class="card-title">User Name : {{$adminData->username}}</h4>
 
                                         <a href="{{ route('edit.profile') }}" class="btn btn-info btn-rounded waves-effect waves-light" > Edit Profile</a>
                                     </div>
