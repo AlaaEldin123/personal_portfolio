c019feef338775f299808406f13d0870d088fecb
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-02-01 03:10:11 +0400

    Backend Add Multi Image in About Page Part 4

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -103,7 +103,7 @@ class AboutController extends Controller
             'alert-type' => 'success'
         );
          
-        return redirect()->back()->with($notification);
+        return redirect()->route('all.multi.image')->with($notification);
         
      }
 
@@ -115,4 +115,40 @@ class AboutController extends Controller
 
      }// End Method 
 
+
+     public function EditMultiImage($id){
+        $multiImage = MultiImage::findOrFail($id);
+        return view('admin.about_page.edit_multi_image',compact('multiImage'));
+
+
+     }// End Method 
+
+
+     public function UpdateMultiImage(Request $request){
+
+        $multi_image_id = $request->id;
+
+        if ($request->file('multi_image')) {
+            $image = $request->file('multi_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($image)->resize(220,220)->save('upload/multi/'.$name_gen);
+            $save_url = 'upload/multi/'.$name_gen;
+
+            MultiImage::findOrFail($multi_image_id)->update([
+                 
+                'multi_image' => $save_url,
+
+            ]); 
+
+            $notification = array(
+            'message' => 'Multi Image Updated Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.multi.image')->with($notification);
+
+        }
+
+     }
 }
