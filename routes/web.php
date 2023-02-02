0720285dc18b379fd9313972cc1de772c2348dab
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-02-02 09:56:52 +0400

    Frontend Contact Form Setup Part 1

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\BlogController;
+use App\Http\Controllers\Home\ContactController;
 use App\Http\Controllers\Home\FooterController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\Home\PortfolioController;
@@ -126,6 +127,16 @@ Route::controller(BlogCategoryController::class)->group(function () {
  Route::controller(FooterController::class)->group(function () {
     Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
     Route::post('/update/footer', 'UpdateFooter')->name('update.footer');
-   
+});
+
+
+
+
+
+
+
+ // Contact All Route 
+ Route::controller(ContactController::class)->group(function () {
+    Route::get('/contact', 'Contact')->name('contact.me');
      
 });
