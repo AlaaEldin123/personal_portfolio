9489234bbd1e1e270f29ccd77d8f75b8eafabcaf
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-02-02 08:02:22 +0400

    Backend Blog Page Setup Part 7

--- routes/web.php
+++ routes/web.php
@@ -112,5 +112,8 @@ Route::controller(BlogCategoryController::class)->group(function () {
     Route::get('/add/blog', 'AddBlog')->name('add.blog');
     Route::post('/store/blog', 'StoreBlog')->name('store.blog');
     Route::get('/edit/blog/{id}', 'EditBlog')->name('edit.blog');
+    Route::post('/update/blog', 'UpdateBlog')->name('update.blog');
+    Route::get('/delete/blog/{id}', 'DeleteBlog')->name('delete.blog');
+
      
 });
